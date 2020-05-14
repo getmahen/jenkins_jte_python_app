125fe77c146e94d1d0ca40e4cbd6277120fdd062
Mahendranath Rekapally <6005699+getmahen@users.noreply.github.com>
2020-05-14 15:18:15 -0700

    Imported ansible lib

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,7 +1,8 @@
-/*
-  This file will inherit the configurations set via: 
-    https://github.com/steven-terrana/example-jte-configuration/blob/master/pipeline_config.groovy
-*/
+//To reference a specific pipeline template file. This setting overrides the global default pipeline template settings defined in the 
+//Governance tier source code repo (pipeline-configuration/pipeline_config.groovy)
+//pipeline_template = "python_Jenkinsfile"
+
 libraries{
   python
+  ansible
 }
