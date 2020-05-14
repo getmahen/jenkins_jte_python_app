5e99549adf2b2daf1c02d5e8814004c5f6f1963d
Mahendranath Rekapally <6005699+getmahen@users.noreply.github.com>
2020-05-13 20:59:31 -0700

    added pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,7 @@
+/*
+  This file will inherit the configurations set via: 
+    https://github.com/steven-terrana/example-jte-configuration/blob/master/pipeline_config.groovy
+*/
+libraries{
+  python
+}
